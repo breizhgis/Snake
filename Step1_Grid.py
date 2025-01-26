5a6a2503c96e086070c7d766ef2dbc45e1a5da53
Eileen Kerhouant <78910775+breizhgis@users.noreply.github.com>
2025-01-26 10:48:14 +0100

    Trying to now make the game grid where a given list of coordinates will show up as an X on the grid.  This 1st try is NOT successful.

--- Step1_Grid.py
+++ Step1_Grid.py
@@ -14,7 +14,28 @@ print('The Pyladies\' Slither Sisters present: ')
 print_grid(grid)
 print('Welcome to our version of Snake!')
 
+# Create a function to make actual game grid.  Start with code that was already written above.
+def game_grid(rows=10, cols=10):
+    grid = [['.' for _ in range(cols)] for _ in range(rows)]
+    snake_body = [] # Empty list as coordinates will be defined outside of the function itself
 
+    for row in range(rows):
+        for col in range(cols):
+            if (row + col) == snake_body:  
+                grid[row][col] = 'X'
+            else:
+                grid[row][col] = '.'
+    return grid
 
+def print_grid(grid):
+    for row in grid:
+        print(' '.join(row))
+
+grid = game_grid()
+snake_body = [(0,0),(1,0),(2,2),(4,3),(8,9),(9,9)]
+print_grid(grid)
+ 
+    
+    
 
 
