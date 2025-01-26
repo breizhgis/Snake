bbb1e9f2454d206e9b6d591beec6aba3f4c893f4
Eileen Kerhouant <78910775+breizhgis@users.noreply.github.com>
2025-01-26 10:10:54 +0100

    Adding some annotation to understand exactly what is being done (and why) at each stage.

--- Step1_Grid.py
+++ Step1_Grid.py
@@ -1,12 +1,14 @@
+# Create a function to make a blank grid (just dots)
 def initial_grid(rows=10, cols=10):
-    # Initialize the grid with dots
     grid = [['.' for _ in range(cols)] for _ in range(rows)]
     return grid
 
+# Include this 2nd function to visualise the blank grid
 def print_grid(grid):
     for row in grid:
         print(' '.join(row))
 
+# Want to see what the previous 2 functions did.  Plus, personalise the presentation a bit.
 grid = initial_grid()
 print('The Pyladies\' Slither Sisters present: ')
 print_grid(grid)
